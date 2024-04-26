b43261cf5d18d0bbe5abdeb782ae3c67288e70e2
Luiskd1 <125316846+Luiskd1@users.noreply.github.com>
2024-04-26 16:57:02 -0500

    ASDF

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,47 +1,42 @@
+'use client'
 import React from "react";
-import {Tabs, Tab} from "@nextui-org/react";
-
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/tabs/tabs";
 
 const page = () => {
-  return (
-    <div className="flex">
-      <div className="">
 
-export default function App() {
+
+
   return (
-    <div className="flex w-full flex-col">
-      <Tabs aria-label="Options" color="primary" variant="bordered">
-        <Tab
-          key="photos"
-          title={
-            <div className="flex items-center space-x-2">
-              <span>Photos</span>
-            </div>
-          }
-        />
-        <Tab
-          key="music"
-          title={
-            <div className="flex items-center space-x-2">
-              <span>Music</span>
-            </div>
-          }
-        />
-        <Tab
-          key="videos"
-          title={
-            <div className="flex items-center space-x-2">
-              <span>Videos</span>
-            </div>
-          }
-        />
-      </Tabs>
-    </div>  
-  );
-}
+    <div className="  flex  h-full"  >
+      <div className="flex-col p-8 gap-6 h-full border border-[#1E2D3D]  ">
+        <Image src='devcode.svg' alt="imagen devcode" width={40} height={40} className="mb-10" />
+        <Image src='personalinfo.svg' alt="imagen personalinfo" width={40} height={40} className="mb-10" />
+        <Image src='hobbiesicon.svg' alt="imagen hobbiesicon" width={40} height={40} />
       </div>
-      
-    </div>
+
+
+      <Tabs defaultValue="account" className="w-full flex ">
+
+        <div>
+          <Accordion>
+            <AccordionItem key="1" aria-label="Personal-Info" title="Personal-Info">
+              <TabsList>
+                <TabsTrigger value="account">Account</TabsTrigger>
+                <TabsTrigger value="password">Password</TabsTrigger>
+              </TabsList>
+            </AccordionItem>
+          </Accordion>
+        </div>
+
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+
+    </div >
+
+
   )
 }
 
