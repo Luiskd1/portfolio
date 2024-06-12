2589430287de83878e7a08b19b63d71e1922a4b3
Luiskd1 <125316846+Luiskd1@users.noreply.github.com>
2024-06-12 14:36:32 -0500

    Update header.tsx

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -17,14 +17,14 @@ const HeaderPage = () => {
         <div className="flex">
           <h1 className="size-5 xl:w-96 w-full h-full font-medium border border-[#1E2D3D]  flex justify-start px-12 items-center mr">Luis Santana</h1>
           <div className="flex">
-            <Link className={pathname === '/' ? 'border-b-[#FEA55F] text-white size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/' >
+            <Link className={pathname === '/' ? 'border-b-[#FEA55F] text-white size-5 w-32 xl:w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/' >
               <motion.div className="underline" layoutId="underline" />_Hello
             </Link>
-            <Link className={pathname === '/about' ? 'border-b-[#FEA55F] text-white size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/about' > _About-me</Link>
-            <Link className={pathname === '/projects' ? 'border-b-[#FEA55F] text-white size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/projects' > _Projects</Link>
+            <Link className={pathname === '/about' ? 'border-b-[#FEA55F] text-white size-5 w-32 xl:w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-32 xl:w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/about' > _About-me</Link>
+            <Link className={pathname === '/projects' ? 'border-b-[#FEA55F] text-white size-5 w-32 xl:w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-32 xl:w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/projects' > _Projects</Link>
           </div>
         </div>
-        <Link className={pathname === '/contact' ? ' text-white size-5 w-48 h-full font-medium border border-[#1E2D3D] border-b-[#FEA55F]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/contact' > _Contact-me</Link>
+        <Link className={pathname === '/contact' ? ' text-white size-5 w-48 h-full font-medium border border-[#1E2D3D] border-b-[#FEA55F]  flex justify-center items-center  px-8 hover:text-white  ' : "  size-5 w-32 xl:w-48 h-full font-medium border border-[#1E2D3D]  flex justify-center items-center  px-8 hover:text-white"} href='/contact' > _Contact-me</Link>
       </div>
 
 
